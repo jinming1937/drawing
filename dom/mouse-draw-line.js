b6e237e17ab4b447c46083f29e37dc61f886bb3f
jinming.1937 <jinming.1937@bytedance.com>
2021-09-06 20:12:59 +0800

    back forward

--- dom/mouse-draw-line.js
+++ dom/mouse-draw-line.js
@@ -1,4 +1,5 @@
 ;(function(w) {
+  w.lines = [];
   /**
    * 绑定事件 & 返回绘画函数
    * @param {Object} ctx canvas context
@@ -12,7 +13,7 @@
     const axisX = domWidth / 2;
     const axisY = domHeight / 2;
 
-    const lines = [];
+    const lines = w.lines;
     let currentLineIndex = lines.length;
     dom.addEventListener('mousedown', (e) => {
       currentLineIndex = lines.length;
@@ -38,35 +39,7 @@
       lines[currentLineIndex][3] = e.layerY * 2 - axisY;
       currentLineIndex = lines.length; // 本次绘画结束，index 前移
     });
-
-    return () => drawLine(ctx, lines);
   }
 
-  /**
-   * 画线段
-   * @param {Object} ctx canvas context
-   * @param {Array} lines line array
-   * @returns 
-   */
-  function drawLine(ctx, lines) {
-    if (lines.length < 0) return;
-    const dom = ctx.canvas;
-    const domWidth = dom.width;
-    const domHeight = dom.height;
-    const axisX = domWidth / 2;
-    const axisY = domHeight / 2;
-    ctx.save();
-    ctx.translate(axisX, axisY);
-    ctx.beginPath();
-    lines.forEach((item) => {
-      ctx.moveTo(item[0], item[1]);
-      ctx.lineTo(item[2], item[3]);
-    })
-    ctx.strokeStyle = '#f5f5f5';
-    ctx.closePath();
-    ctx.stroke();
-    ctx.restore();
-  }
-
-  window.mouseDrawLine = draw;
+  w.mouseDrawLine = draw;
 }(window));
---
+++ draw/line.js
@@ -0,0 +1,30 @@
+;(function(w) {
+
+  /**
+   * 画线段
+   * @param {Object} ctx canvas context
+   * @param {Array} lines line array
+   * @returns 
+   */
+   function drawLine(ctx, lines) {
+    if (lines.length < 0) return;
+    const dom = ctx.canvas;
+    const domWidth = dom.width;
+    const domHeight = dom.height;
+    const axisX = domWidth / 2;
+    const axisY = domHeight / 2;
+    ctx.save();
+    ctx.translate(axisX, axisY);
+    ctx.beginPath();
+    lines.forEach((item) => {
+      ctx.moveTo(item[0], item[1]);
+      ctx.lineTo(item[2], item[3]);
+    })
+    ctx.strokeStyle = '#f5f5f5';
+    ctx.closePath();
+    ctx.stroke();
+    ctx.restore();
+  }
+
+  w.drawLine = drawLine
+}(window));

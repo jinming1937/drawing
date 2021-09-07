8ccda20fae6f130db50ce49df93809d130413b6c
jinming.1937 <jinming.1937@bytedance.com>
2021-09-07 11:57:52 +0800

    down pic

--- dom/export-picture.js
+++ dom/export-picture.js
@@ -2,10 +2,14 @@
   function init(canvas) {
     const dom = document.getElementById('export-picture');
     dom.addEventListener('click', () => {
-      canvas.toDataURL('image/png', 0);
+      const image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+      window.location.href = image; // it will save locally
+
+      // var image = canvas.toDataURL("image/png");
+      // var w = window.open('about:blank','image from canvas');
+      // w.document.write("<img src='"+image+"' alt='from canvas'/>");
     });
   }
 
   w.initExport = init;
-
 }(window));

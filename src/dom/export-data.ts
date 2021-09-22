3677737075a6003a8d2f7d678d8c5414d9c219cf
jinming.1937 <jinming.1937@bytedance.com>
2021-09-22 20:05:10 +0800

    pen bounce

--- src/dom/export-data.ts
+++ src/dom/export-data.ts
@@ -1,3 +1,4 @@
+import { IDrawData } from '.';
 import {drawData} from './mouse-draw';
 
 function exportRaw(w: Window, name: string, data: string) {
@@ -9,11 +10,28 @@ function exportRaw(w: Window, name: string, data: string) {
   save_link.click();
 }
 
+function filterData(data: IDrawData[]) {
+  return data.filter((item) => {
+    switch(item.type) {
+      case 'txt':
+        return !!item.text;
+      case 'line':
+      case 'rect':
+      case 'arrow':
+        const [x1, y1, x2, y2] = item.shape;
+        return item.shape.length === 4 && x1 !== x2 && y1 !== y2;
+      case 'pen':
+        return item.lines.filter(i => i.length === 2).length > 0;
+    }
+  })
+}
+
 export function initExportData(w: Window) {
   const dom = document.getElementById('export-data');
   if (dom) {
     dom.addEventListener('click', () => {
-      exportRaw(w, `draw_data_${+new Date()}.json`, JSON.stringify(drawData));
+      const simpleData = filterData(drawData);
+      exportRaw(w, `draw_data_${+new Date()}.json`, JSON.stringify(simpleData));
     });
   }
 }

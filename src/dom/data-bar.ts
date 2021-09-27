36928270ba8a3277f25d63e3b7ce046040de1b4c
jinming <jinming1937@163.com>
2021-09-27 10:34:02 +0800

    add right bar

---
+++ src/dom/data-bar.ts
@@ -0,0 +1,13 @@
+/**
+ * right bar
+ * show data
+ */
+
+export function initRightBar() {
+  const div = document.createElement('div')
+  div.id = 'right-bar';
+
+  div.className = 'right-bar';
+
+  document.body.append(div);
+}
